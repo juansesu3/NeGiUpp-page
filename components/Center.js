1317458124422eca4c818e56cf1e44b5ef1127f5
juansesu3 <juanseu3@hotmail.com>
2023-06-22 11:35:10 +0200

    working on archiquetecture and styles

---
+++ components/Center.js
@@ -0,0 +1,16 @@
+import { styled } from "styled-components"
+
+const StyledDiv = styled.div`
+max-width: 800px;
+margin: 0 20px;
+
+
+`;
+
+const Center = ({children}) => {
+  return (
+    <StyledDiv>{children}</StyledDiv>
+  )
+}
+
+export default Center
--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,40 @@
 import Link from "next/link";
+import { styled } from "styled-components";
+import Center from "./Center";
+
+const StyledHeader = styled.header`
+  background-color: #0f0f0f;
+`;
+
+const LogoLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+`;
+
+const Logo = styled.img`
+  width: 50px;
+`;
 
 const Header = () => {
   return (
-    <header>
-      <Link href={"/"}>
-        <img
-          style={{ width: "50px" }}
-          src="https://my-page-negiupp.s3.amazonaws.com/1687424109024.png"
-          alt="logo-image"
-        />
-      </Link>
-      <nav>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/about"}>About</Link>
-        <Link href={"/proyects"}>Proyects</Link>
-        <Link href={"/blog"}>Blog</Link>
-        <Link href={"/contact"}>Contact</Link>
-      </nav>
-      <Link href={"/contact"}> Let&apos;s talk</Link>
-    </header>
+    <StyledHeader>
+      <Center>
+        <LogoLink href={"/"}>
+          <Logo
+            src="https://my-page-negiupp.s3.amazonaws.com/1687424109024.png"
+            alt="logo-image"
+          />
+        </LogoLink>
+        <nav>
+          <Link href={"/"}>Home</Link>
+          <Link href={"/about"}>About</Link>
+          <Link href={"/proyects"}>Proyects</Link>
+          <Link href={"/blog"}>Blog</Link>
+          <Link href={"/contact"}>Contact</Link>
+        </nav>
+        <Link href={"/contact"}> Let&apos;s talk</Link>
+      </Center>
+    </StyledHeader>
   );
 };
 
