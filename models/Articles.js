f359f2603ba0cffaaee511f1f8b5445ba8fafb8d
juansesu3 <juanseu3@hotmail.com>
2023-06-26 14:20:31 +0200

    feature(fetching articles)

---
+++ models/Articles.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const ArticleSchema = new Schema({
+  title: { type: String, require: true },
+  summary: { type: String, require: true },
+  content: { type: String, require: true },
+  author: { type: String, require: true },
+  imgAuthor: { type: String, require: true },
+  images: [{ type: [String], require: true }],
+  articleCat: { type: mongoose.Types.ObjectId, ref: "CategoryArticle" },
+  topics: {type: Object},
+});
+
+export const Article = models.Article || model("Article", ArticleSchema);
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import Header from "@/components/Header";
 import Home from "@/components/Home";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Article } from "@/models/Articles";
 import { styled } from "styled-components";
 
 
-const HomePage = () => {
+const HomePage = ({article}) => {
+
   return ( 
     <div>
       <Header />
-      <Home />
+      <Home article={article} />
     </div> 
   );
 };
+
+export const getServerSideProps = async()=>{
+  const feuturedArticle = "64997ffb602af53155296e81";
+  await mongooseConnect();
+  const article = await Article.findById(feuturedArticle);
+
+  return {
+    props: {article: JSON.parse(JSON.stringify(article))},
+  }
+
+}
+
 export default HomePage;
