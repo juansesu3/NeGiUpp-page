ffef8acef38ab58f0dbd83d29976d73f929634fe
juansesu3 <juanseu3@hotmail.com>
2023-06-29 11:23:28 +0200

    fix(bug) I got a problem to gice the styles for diferent instancs of the one component ProyectsCard so I was refactorized my code and integrated the page  of proyects with cards and i can develop my own design

--- pages/proyects.js
+++ pages/proyects.js
@@ -1,9 +1,56 @@
 import Center from "@/components/Center";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import ProyectCard from "@/components/ProyectCard";
+
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+
+const ContainerAllProyects = styled.div`
+  display: flex;
+  gap: 1.5rem;
+`;
+
+const ContainerProyectL = styled(Link)`
+  text-decoration: none;
+  color: #ffff;
+  width: 100%;
+`;
+
+const FirstProyectsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+
+const SecondProyectsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem;
+`;
+
+const ContainerPro = styled.div`
+  border-radius: 1.5rem;
+  padding: 1rem;
+  width: 20rem;
+  background: linear-gradient(
+    100deg,
+    rgb(189 189 189 / 7%),
+    rgba(255, 255, 255, 0)
+  );
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2),
+    /* Sombra superior */ 0 8px 16px rgba(0, 0, 0, 0.4),
+    /* Sombra inferior */ 4px 0 4px rgba(0, 0, 0, 0.1),
+    /* Sombra derecha */ -4px 0 4px rgba(0, 0, 0, 0.1); /* Sombra izquierda */
+`;
+
+const ImageContainer = styled.div`
+  img {
+    width: 100%;
+    border-radius: 1.5rem;
+  }
+`;
 
 const ProyectsPage = () => {
   const [proyects, setProyects] = useState([]);
@@ -14,12 +61,49 @@ const ProyectsPage = () => {
     });
   }, []);
 
+  const FirstProyects = proyects.slice(0, 2);
+
+  const SecondProyects = proyects.slice(2);
+
   return (
     <div>
       <Header />
       <Center>
-        <h1 style={{color:"#ffff"}}>Proyects</h1>
-        <ProyectCard proyects={proyects} />
+        <ContainerAllProyects>
+          <FirstProyectsContainer>
+            {FirstProyects.length > 0 &&
+              FirstProyects.map((proyect) => (
+                <ContainerPro key={proyect._id}>
+                  <ContainerProyectL href={"/"}>
+                    <ImageContainer>
+                      <img src={proyect.images[0]} alt="proyect image" />
+                    </ImageContainer>
+                    <div>
+                      <h1>{proyect.title}</h1>
+                    </div>
+                  </ContainerProyectL>
+                </ContainerPro>
+              ))}
+          </FirstProyectsContainer>
+          <div>
+            <h1>All proyects</h1>
+            <SecondProyectsContainer>
+              {SecondProyects.length > 0 &&
+                SecondProyects.map((proyect) => (
+                  <ContainerPro key={proyect._id}>
+                    <ContainerProyectL href={"/"}>
+                      <ImageContainer>
+                        <img src={proyect.images[0]} alt="proyect image" />
+                      </ImageContainer>
+                      <div>
+                        <h1>{proyect.title}</h1>
+                      </div>
+                    </ContainerProyectL>
+                  </ContainerPro>
+                ))}
+            </SecondProyectsContainer>
+          </div>
+        </ContainerAllProyects>
       </Center>
       <Footer />
     </div>
