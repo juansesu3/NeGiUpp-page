c5a0465717181348b5a35d1d27c1f741b6c180b8
juansesu3 <juanseu3@hotmail.com>
2023-10-19 16:45:23 +0200

    FEATURE(Google analitics)

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-W3WEF7VKMS"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-W3WEF7VKMS');
+            `,
+          }}
+        />
+        {/* Fin Google Analytics */}
+      </Head>
       <body>
         <Main />
         <NextScript />
