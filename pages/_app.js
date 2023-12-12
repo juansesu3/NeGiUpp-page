333c4bd936c6bbc11de886b445a513e65931344a
juansesu3 <juanseu3@hotmail.com>
2023-12-12 16:23:53 +0100

    FEATURE(improved SEO)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { SessionProvider } from "next-auth/react";
 import "../styles/fonts.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps: { session, ...pageProps }, }) {
   const [isMounted, setIsMounted] = useState(false);
@@ -14,6 +15,12 @@ export default function App({ Component, pageProps: { session, ...pageProps }, }
   return (
     <>  
     <SessionProvider session={session}>
+    <Head>
+          <meta name="description" content="Full-stack web developer specialized in Next.js, React.js, JavaScript, and TypeScript. Creator of digital products with high-quality standards under the Negiupp brand. Explore my services, projects, and the 'print on demand' process in my ecommerce with Shopify and Printify." />
+          <meta property="og:image" content="https://my-page-negiupp.s3.amazonaws.com/1702394288820.jpeg" />
+          <meta property="og:image:width" content="300" />
+          <meta property="og:image:height" content="200" />
+        </Head>
       <Component {...pageProps} />
     </SessionProvider>
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,10 @@ export default function Document() {
           }}
         />
         {/* Fin Google Analytics */}
+        <meta name="description" content="Full-stack web developer specialized in Next.js, React.js, JavaScript, and TypeScript. Creator of digital products with high-quality standards under the Negiupp brand. Explore my services, projects, and the 'print on demand' process in my ecommerce with Shopify and Printify." />
+          <meta property="og:image" content="https://my-page-negiupp.s3.amazonaws.com/1702394288820.jpeg" />
+          <meta property="og:image:width" content="500" />
+          <meta property="og:image:height" content="200" />
       </Head>
       <body>
         <Main />
