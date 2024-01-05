13d511d5930295b27b571a11803f453d2da62d42
juansesu3 <juanseu3@hotmail.com>
2024-01-05 08:08:30 +0100

    FEATURE(email marketing)

--- pages/api/sendComfirmation.js
+++ pages/api/sendComfirmation.js
@@ -19,7 +19,7 @@ const handler = async (req, res) => {
                     <div style="background-color: #f0f0f0; padding: 20px; text-align: center;">
                         <h1 style="color: #00365c;">Thanks for signing up!</h1>
                         <p style="font-size: 16px;">Please click the button below to confirm your subscription 🤗</p>
-                        <a href="http://localhost:3000/confirm-subscription/token=${userId}" style="background-color: #00365c; text-decoration:none; color: #fff; padding: 10px; border: none; border-radius: 5px; cursor: pointer;">             
+                        <a href="${process.env.BASE_URL}/confirm-subscription/token=${userId}" style="background-color: #00365c; text-decoration:none; color: #fff; padding: 10px; border: none; border-radius: 5px; cursor: pointer;">             
                                 I confirm my subscription!
                         </a>
                         <p>You can also use this link: ${process.env.BASE_URL}/confirm-subscription/token=${userId}</p>
