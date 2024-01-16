098f5bec14bfa61ad9f00e935c225c7ba6359d2e
juansesu3 <juanseu3@hotmail.com>
2024-01-16 10:02:26 +0100

    FEATURE(new)

--- pages/links.js
+++ pages/links.js
@@ -150,13 +150,11 @@ svg{
 `;
 
 const ContainerIconsLinks = styled.div`
-
 fill: white;
-
 display: flex;
 align-items: center;
 justify-content: center;
-margin-top: 2rem;
+margin: 2rem 0;
 gap: 1rem;
 cursor: pointer;
 svg{
@@ -194,9 +192,12 @@ const LinksPage = () => {
     }, []);
 
     const handleClickPoints = () => {
-        alert('share')
+        const links = 'https://negiupp.com/links'
+        Swal.fire({
+            html: customContent,
+        }); 
+        nodeListener(links)
     }
-
 
     const compartirEnFacebook = (link) => {
         const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(link)}`;
@@ -256,7 +257,7 @@ const LinksPage = () => {
     <h1 class="custom-title">Share this link</h1>
         <!-- Repite el mismo formato para los otros 6 contenedores -->
     <!-- ... -->
-    <button class="custom-item" type="button" data-funtion="compartirEnRedSocial('facebook')" >
+    <div class="custom-item" type="button" data-funtion="compartirEnRedSocial('facebook')" >
     <div class="info">
     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
     <path fill="#3F51B5" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path><path fill="#FFF" d="M34.368,25H31v13h-5V25h-3v-4h3v-2.41c0.002-3.508,1.459-5.59,5.592-5.59H35v4h-2.287C31.104,17,31,17.6,31,18.723V21h4L34.368,25z"></path>
@@ -268,7 +269,7 @@ const LinksPage = () => {
         <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
     </svg>
     </div>
-    </button>
+    </div>
     <!-- Repite el mismo formato para los otros 6 contenedores -->
     <!-- ... -->
     <div class="custom-item" type="button" data-funtion="compartirEnRedSocial('linkedin')">
